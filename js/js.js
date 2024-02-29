7a419a0578797ce84df6d474070341b0108700d9
Abdallah2132000 <111452593+Abdallah2132000@users.noreply.github.com>
2024-02-29 13:03:12 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+let img = document.querySelector(".imgeicon")
+let container = document.querySelector(".container")
+
+function watchs(watch){
+    img.src = watch;
+}
+
+function color(light){
+    container.style.background = light
+
+}
